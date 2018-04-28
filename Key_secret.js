d057ba9b02570db717b4a4640caa56c344262f59
Maggico <lupodimare87@gmail.com>
2018-04-28 19:40:25 +0200

    primo caricamento

---
+++ Key_secret.js
@@ -0,0 +1,7 @@
+module.exports = {
+  consumer_key:         'TkkLkEkV3tOlhoR3FGvMKNjIl',
+  consumer_secret:      'pOrdbFKkOSpMCl8pmiQTS7yHWBA2Rr1QnjB3iZwY6xPfzLcFZC',
+  access_token:         '920917613655584768-CKQaMPTEbWAeSrQH8q8MtKOcP1FY0fI',
+  access_token_secret:  'ubgDXk4q0cbIXsWePcq5vnWW5qwNNz7n6VjxV03Dcj6sp',
+  // timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+}
